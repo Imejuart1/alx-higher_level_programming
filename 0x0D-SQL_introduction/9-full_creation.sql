8e39e176540a9a830ed362ddc4236d30b2f41e27
Imejuart1 <101446446+Imejuart1@users.noreply.github.com>
2022-09-08 18:22:35 +0100

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,20 @@
--- displays the number of records with id = 89
-SELECT COUNT(first_table.id) FROM first_table WHERE id=89
+-- creates a table second_table
+CREATE TABLE IF NOT EXISTS second_table
+(
+	id INT,
+       	name VARCHAR(256),
+       	score INT
+);
+INSERT INTO second_table (id, name, score) VALUES
+(
+	1, "John", 10
+),
+(
+	2, "Alex", 3
+),
+(
+	3, "Bob", 14
+),
+(
+	4, "George", 8
+)
